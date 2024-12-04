e79bcee519828f97cd2377e9c25adfcbebc1998f
Siirko <alexander.yanovskyy@gmail.com>
2024-12-04 14:15:57 +0100

    silence warning and fix emsdk versioin

--- scripts/build.sh
+++ scripts/build.sh
@@ -2,6 +2,9 @@
 
 cd "`dirname \"$0\"`/.."
 
+emsdk install 3.1.61
+emsdk activate 3.1.61
+
 emmake make clean
 emconfigure autoreconf -fiv
 ac_cv_exeext=".html" emconfigure ./configure --host=none-none-none
